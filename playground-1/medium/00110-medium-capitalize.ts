ea60e8b190451b4c29a79a07677f1f9aac5b442f
gengdz <1583751445@qq.com>
2024-12-02 14:22:25 +0800

    feat: capitalize

--- playground-1/medium/00110-medium-capitalize.ts
+++ playground-1/medium/00110-medium-capitalize.ts
@@ -18,7 +18,7 @@
 
 /* _____________ 你的代码 _____________ */
 
-type MyCapitalize<S extends string> = any
+type MyCapitalize<S extends string> = S extends `${infer F}${infer E}` ? `${Uppercase<F>}${E}` : S
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
